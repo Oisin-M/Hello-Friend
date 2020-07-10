816937366851d8a44582a154e4a9e966e2f68273
Oisin-M <oisinmmm@gmail.com>
2020-07-10 21:02:23 +0100

    redirects to /browse when logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
 
   def index
+    redirect_to browse_path if account_signed_in?
   end
-  
+
   def browse
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'home/browse'
-  get 'home/index'
+
   resources :messages
   resources :conversations
   resources :projects
   devise_for :accounts
+
   root 'home#index'
+  get '/browse', to: "home#browse", as: "browse"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
