c0b3c6470855a53c70113ca4af382c97e0f5ff95
Oisin-M <oisinmmm@gmail.com>
2020-07-11 11:59:12 +0100

    added some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Account.create([
+   {email: "S.Murphy88@nuigalway.ie", password: "Password"},
+   {email: "B.OSullivan18@nuigalway.ie", password: "Password"},
+   {email: "O.Morrison1@nuigalway.ie", password: "Password"},
+   {email: "B.ODonnell15@nuigalway.ie", password: "Password"},
+   {email: "C.Kavanagh26@nuigalway.ie", password: "Password"}
+  ])
+
+
+
+Project.create([
+  {account_id: 1, title: "CERN Webfest Hackathon", description: "Help us make a better future!"},
+  {account_id: 2, title: "VisuPy", description: "Visualise Python Functions"},
+  {account_id: 3, title: "Less Typing, More Thinking!", description: "Make a Latex database"},
+  {account_id: 4, title: "CitiCERN", description: "Building cities for the future"},
+  {account_id: 5, title: "DisCERN", description: "Join us and let's make something!"},
+  {account_id: 3, title: "Hello, friend", description: "Bringing programmers together"},
+])
