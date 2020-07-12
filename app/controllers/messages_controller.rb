2554279bb28724a77a95e2b08bf3f9d952bcf23c
Oisin-M <oisinmmm@gmail.com>
2020-07-12 14:10:44 +0100

    Basic functionality is now all working!

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -28,7 +28,7 @@ class MessagesController < ApplicationController
 
     @message.save
 
-    ActionCable.server.broadcast("conversation_channel_#{@message.swipe_id}", message: @message.body, sender: Account.find(current_account.id).email)
+    ActionCable.server.broadcast("conversation_channel_#{@message.swipe_id}", message: @message.body, sender: Account.find(current_account.id).email, sender_id: current_account.id)
 
     redirect_to Swipe.find(@message.swipe_id)
 
--- app/javascript/channels/conversation_channel.js
+++ app/javascript/channels/conversation_channel.js
@@ -5,6 +5,9 @@ document.addEventListener('turbolinks:load', () => {
   const swipe_element = document.getElementById('swipe_id');
   const swipe_id = swipe_element.getAttribute('data-swipe-id');
 
+  const acc_element = document.getElementById('acc_id');
+  const acc_id = acc_element.getAttribute('data-acc-id');
+
   consumer.subscriptions.create({ channel: "ConversationChannel", swipe_id: swipe_id}, {
     connected() {
       // Called when the subscription is ready for use on the server
@@ -18,11 +21,16 @@ document.addEventListener('turbolinks:load', () => {
     received(data) {
       // Called when there's incoming data on the websocket for this channel
       console.log(data);
-      const message_div = document.getElementById("messages");
-      message_div.innerHTML+=data.message
-      message_div.innerHTML+=" - "
-      message_div.innerHTML+=data.sender
-      message_div.innerHTML+="</br>"
+      if (acc_id==data.sender_id) {
+
+      }
+      else {
+        const message_div = document.getElementById("messages");
+        message_div.innerHTML+=data.message
+        message_div.innerHTML+=" - "
+        message_div.innerHTML+=data.sender
+        message_div.innerHTML+="</br>"
+      }
     }
   });
 
