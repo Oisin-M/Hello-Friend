ee71a9d9ce51b38c537ce5d4c7c07edbb10be37b
Oisin-M <oisinmmm@gmail.com>
2020-07-13 00:18:36 +0100

    can now edit profiles from browse page, next allow projects creation

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -30,6 +30,19 @@ class ProfilesController < ApplicationController
 
   # GET /profiles/1/edit
   def edit
+
+    @my_projects=Project.where(account_id: current_account.id)
+    @seen = Swipe.where(account_id: current_account.id)
+    @likes = @seen.where(liked: true)
+    @liked_project_ids = []
+    @likes.each do |like|
+      @liked_project_ids.append(like.project_id)
+    end
+    @liked_projects = Project.where(id: @liked_project_ids)
+
+    @edit_profile = "not null"
+
+    render "home/browse"
   end
 
   # POST /profiles
