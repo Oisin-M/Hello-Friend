f51bfe7872ef27e052b8e937826e7470738187fb
Oisin-M <oisinmmm@gmail.com>
2020-09-09 11:50:17 +0100

    can now create projects on the browse template correctly

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,6 +7,25 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def new_project
+
+    @my_projects=Project.where(account_id: current_account.id)
+    @seen = Swipe.where(account_id: current_account.id)
+    @likes = @seen.where(liked: true)
+    @liked_project_ids = []
+    @likes.each do |like|
+      @liked_project_ids.append(like.project_id)
+    end
+    @liked_projects = Project.where(id: @liked_project_ids)
+
+    @my_profile=Profile.where(account_id: current_account.id).first
+
+    @new_project = "not null"
+
+    render "home/browse"
+  end
+
+
   # GET /projects/1
   # GET /projects/1.json
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/browse', to: "home#browse", as: "browse"
 
+  get 'new_project' => "projects#new_project", as: "new"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
