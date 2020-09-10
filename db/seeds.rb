eb0560d9a05221e49aa015bf58ba3e23f3e066eb
Oisin-M <oisinmmm@gmail.com>
2020-09-10 19:48:18 +0100

    removed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Account.create([
-   {email: "S.Murphy88@nuigalway.ie", password: "Password"},
-   {email: "B.OSullivan18@nuigalway.ie", password: "Password"},
-   {email: "O.Morrison1@nuigalway.ie", password: "Password"},
-   {email: "B.ODonnell15@nuigalway.ie", password: "Password"},
-   {email: "C.Kavanagh26@nuigalway.ie", password: "Password"}
-  ])
-
-
-
-Project.create([
-  {account_id: 1, title: "CERN Webfest Hackathon", description: "Help us make a better future!", skills: "Java", category: "Hackathon"},
-  {account_id: 2, title: "VisuPy", description: "Visualise Python Functions", skills: "Javascript", category: "Humanitarian"},
-  {account_id: 3, title: "Less Typing, More Thinking!", description: "Make a Latex database", skills: "Python", category: "Science"},
-  {account_id: 4, title: "CitiCERN", description: "Building cities for the future", skills: "Scratch", category: "Algorithms"},
-  {account_id: 5, title: "DisCERN", description: "Join us and let's make something!", skills: "C#", category: "Manufacture"},
-  {account_id: 3, title: "Hello, friend", description: "Bringing programmers together", skills: "C++", category: "Collaboration"},
-])
